44c5cade97e1aeadfb5811df4f59c9bf8e7cd29b
Liam Townsley <contact@liamtownsley.me>
2022-08-02 19:12:52 +0100

    feat: adding sample e-mail

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-      fontFamily: {
-        'google-sans': ['"Product Sans"', 'sans-serif']
+    fontFamily: {
+      'google-sans': ['"Product Sans"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary-1': '#f6f8fc',
+        'primary-2': '#c2e7ff',
+        'secondary-1': '#202124',
+        'secondary-2': '#eef3fc',
+        'secondary-3': '#d3e3fd',
+        'secondary-4': 'rgba(32, 33, 36, 0.06)',
+        'tertiary-1': '#cccfd4'
       },
-      extend: {
-        colors: {
-          'primary-1': '#f6f8fc',
-          'primary-2': '#c2e7ff',
-          'secondary-1': '#202124',
-          'secondary-2': '#eef3fc',
-          'secondary-3': '#d3e3fd',
-          'secondary-4': 'rgba(32, 33, 36, 0.06)'
-        },
-      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
@@ -22,4 +23,9 @@ module.exports = {
       addVariant('child-hover', '& > *:hover');
     }
   ],
+  variants: {
+    extend: {
+      visibility: ["group-hover"],
+    },
+  },
 }
